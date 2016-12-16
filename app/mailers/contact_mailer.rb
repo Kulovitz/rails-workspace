4e6bca6d04bd9562ee962a59992adc7febac84b5
Johnathan Kulovitz <jck83344@uga.edu>
2016-12-16 16:41:08 +0000

    Added Mailer functionality

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ActionMailer::Base
-        default to: 'kulovitz@uga.edu'
-        def contact_email(name, email, body)
-            @name = name
-            @email = email
-            @body = body
-            mail(from: email, subject: 'Contact Form Message')
-        end
+        default to: 'jkulovitz@gmail.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        mail(from: email, subject: 'Contact Form Message')
     end
+end
